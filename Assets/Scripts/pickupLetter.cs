7644ba9da5aa092e49755e8b256228d5d6a17541
Alifandra Moamar Farizy <alifandramf16@gmail.com>
2026-07-04 21:32:23 +0700

    Added main gameplay (collect item)
    This is a main gameplay, I've added some following features:
    1. Collect main object
    
    next I'll add obj and sounds

---
+++ Assets/Scripts/pickupLetter.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class pickupLetter : MonoBehaviour
+{
+    public GameObject collectTextObj, intText;
+    public AudioSource pickupSound, ambianceLayer1, ambianceLayer2, ambianceLayer3, ambianceLayer4, ambianceLayer5, ambianceLayer6, ambianceLayer7, ambianceLayer8;
+    public bool interactable;
+    public static int pagesCollected;
+    public Text collectText;
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("MainCamera"))
+        {
+            intText.SetActive(true);
+            interactable = true;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("MainCamera"))
+        {
+            intText.SetActive(false);
+            interactable = false;
+        }
+    }
+    void Update()
+    {
+        if(interactable == true)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                pagesCollected = pagesCollected + 1;
+                collectText.text = pagesCollected + "/8 pages";
+                collectTextObj.SetActive(true);
+                pickupSound.Play();
+                if(pagesCollected == 1)
+                {
+                    ambianceLayer1.Play();
+                }
+                if (pagesCollected == 2)
+                {
+                    ambianceLayer2.Play();
+                }
+                if (pagesCollected == 3)
+                {
+                    ambianceLayer3.Play();
+                }
+                if (pagesCollected == 4)
+                {
+                    ambianceLayer4.Play();
+                }
+                if (pagesCollected == 5)
+                {
+                    ambianceLayer5.Play();
+                }
+                if (pagesCollected == 6)
+                {
+                    ambianceLayer6.Play();
+                }
+                if (pagesCollected == 7)
+                {
+                    ambianceLayer7.Play();
+                }
+                if (pagesCollected == 8)
+                {
+                    ambianceLayer8.Play();
+                }
+                intText.SetActive(false);
+                this.gameObject.SetActive(false);
+                interactable = false;
+            }
+        }
+    }
+}
